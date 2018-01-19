64f4879c0de221d21b693789b4ae78493e7b80e9
Math <roskovyvran@mail.ru>
2018-01-18 22:34:42 -0600

    I had conversion all wrong oh no
    It's fixed now. and the regex actually parses numbers correctly and preserves the rate if there is one.

--- src/commands/spc_conversion.js
+++ src/commands/spc_conversion.js
@@ -1,46 +1,58 @@
-const REGEX = '(\\d+)[\\s\\-]?°?(\\w+)\\.?';
+const MATCH_STRING = '([\\d\\.\\-]+)[\\s\\-]?°?(\\w+)(\\/\\w+)*';
+const ALL_REGEX = new RegExp(MATCH_STRING,'g');
+const ONE_REGEX = new RegExp(MATCH_STRING);
 
-const F = 9/5;
+const F = 1.8;
 const C = 1/F;
 const CM = 2.54; 
 const IN = 1/CM;
-const FT = 12;
-const M = 100;
-const MI = 5280;
-const KM = 1000;
-const L = 3.78541;
-const GAL = 1/L;
+const IN_PER_FT = 12;
+const CM_PER_M = 100;
+const FT_PER_MI = 5280;
+const M_PER_KM = 1000;
+const GAL = 3.78541;
+const L = 1/GAL;
 
 const CONVERSION_TABLE = {
-	'f': function (c) {
-		return 32 + c * F;
+	// Fahrenheit -> Celsius
+	'F': function (x) {
+		return (x - 32) * F;
 	},
-	'c': function (f) {
-		return (f - 32) * C;
+	// Celsius -> Fahrenheit
+	'C': function (x) {
+		return x * C + 32;
 	},
-	'in': function (cm) {
-		return cm * IN;
+	// Inches -> Centimeters
+	'in': function (x) {
+		return x * CM;
 	},
-	'cm': function (inches) {
-		return inches * CM;
+	// Centimeters -> Inches
+	'cm': function (x) {
+		return x * IN;
 	},
-	'ft': function (m) {
-		return (m * M) * (IN / FT);
+	// Feet -> Meters
+	'ft': function (x) {
+		return this['in'](x * IN_PER_FT) / CM_PER_M;
 	},
-	'm': function (ft) {
-		return (ft * FT) * (CM / M);
+	// Meters -> Feet
+	'm': function (x) {
+		return this['cm'](x * CM_PER_M) / IN_PER_FT;
 	},
-	'mi': function (km) {
-		return (km * KM * M) * (IN / FT / MI);
+	// Miles -> Kilometers
+	'mi': function (x) {
+		return this['ft'](x * FT_PER_MI) / M_PER_KM;
 	},
-	'km': function (mi) {
-		return (mi * MI * FT) * (CM / M / KM);
+	// Kilometers -> Miles
+	'km': function (x) {
+		return this['m'](x * M_PER_KM) / FT_PER_MI;
 	},
-	'gal': function (l) {
-		return l * GAL;
+	// Gallons -> Liters
+	'gal': function (x) {
+		return x * GAL;
 	},
-	'L': function (gal) {
-		return gal * L;
+	// Liters -> Gallons
+	'L': function (x) {
+		return x * L;
 	}
 };
 
@@ -62,60 +74,68 @@ module.exports = {
 	},
 	resolver({client, message, userID, channelID}) {
 		this.data.conversions = [];
-		for (let item of message.match(new RegExp(REGEX,'g'))) {
-			var [,value,unit] = item.match(new RegExp(REGEX)));
+		var matches = message.match(ALL_REGEX);
+		if (!matches) {
+			return;
+		}
+		for (let item of matches) {
+			var [,value,unit,rate=''] = item.match(ONE_REGEX);
 			value = Number(value);
+			if (isNaN(value)) {
+				continue;
+			}
 			unit = unit.toLowerCase();
+			rate = rate.toLowerCase();
 			switch (unit) {
 				case 'f':
 				case 'fahrenheit':
-					this.data.conversions.push({from:'f',to:'c',value});
+					this.data.conversions.push({from:'F',to:'C',value,rate});
 					break;
 				case 'c':
 				case 'celsius':
-					this.data.conversions.push({from:'c',to:'f',value});
+					this.data.conversions.push({from:'C',to:'F',value,rate});
 					break;
 				case 'in':
 				case 'inch':
 				case 'inches':
-					this.data.conversions.push({from:'in',to:'cm',value});
+					this.data.conversions.push({from:'in',to:'cm',value,rate});
 					break;
 				case 'cm':
 				case 'centimeter':
 				case 'centimeters':
-					this.data.conversions.push({from:'cm',to:'in',value});
+					this.data.conversions.push({from:'cm',to:'in',value,rate});
 					break;
 				case 'ft':
 				case 'foot':
 				case 'feet':
-					this.data.conversions.push({from:'ft',to:'m',value});
+					this.data.conversions.push({from:'ft',to:'m',value,rate});
 					break;
 				case 'm':
 				case 'meter':
 				case 'meters':
-					this.data.conversions.push({from:'m',to:'ft',value});
+					this.data.conversions.push({from:'m',to:'ft',value,rate});
 					break;
 				case 'mi':
 				case 'mile':
 				case 'miles':
-					this.data.conversions.push({from:'mi',to:'km',value});
+					this.data.conversions.push({from:'mi',to:'km',value,rate});
 					break;
 				case 'km':
 				case 'kilometer':
 				case 'kilometers':
-					this.data.conversions.push({from:'km',to:'mi',value});
+					this.data.conversions.push({from:'km',to:'mi',value,rate});
 					break;
 				case 'gal':
 				case 'gallon':
 				case 'gallons':
-					this.data.conversions.push({from:'gal',to:'L',value});
+					this.data.conversions.push({from:'gal',to:'L',value,rate});
 					break
 				case 'l':
 				case 'liter':
 				case 'litre':
 				case 'liters':
 				case 'litres':
-					this.data.conversions.push({from:'L',to:'gal',value});
+					this.data.conversions.push({from:'L',to:'gal',value,rate});
 					break;
 			}
 		}
@@ -125,8 +145,8 @@ module.exports = {
 	},
 	events: {
 		convert() {
-			return this.data.conversions.map(function ({from,to,value}) {
-				return `${value} ${from} ≈ ${CONVERSION_TABLE[from](value)} ${to}`;
+			return this.data.conversions.map(function ({from,to,value,rate}) {
+				return `${value} ${from}${rate} ≈ ${CONVERSION_TABLE[from](value).toFixed(2)} ${to}${rate}`;
 			}).join('\n');
 		}
 	}
